d008348c7e4118c22d2a83c4a45ccd5405f59b87
Serhii-Semenov <semenov!@mksat.net>
2017-06-06 16:12:21 +0300

    first start

---
+++ ASP.NET/MVC5_1/MVC5_1/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using MVC5_1.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MVC5_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // создаем контекст данных
+        BookContext db = new BookContext();
+
+        public ActionResult Index()
+        {
+            // получаем из бд все объекты Book
+            IEnumerable<Book> books = db.Books;
+            // передаем все объекты в динамическое свойство Books в ViewBag
+            ViewBag.Books = books;
+            // возвращаем представление
+            return View();
+        }
+    }
+}
---
+++ ASP.NET/MVC5_1/MVC5_1/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using MVC5_1.Models;
+using System.Data.Entity;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace MVC5_1
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Database.SetInitializer(new BookDbInitializer());
+
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
