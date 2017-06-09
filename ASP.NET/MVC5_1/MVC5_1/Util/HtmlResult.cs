912e36a72288879d133ecab6b3932ee002050812
Serhii-Semenov <semenov!@mksat.net>
2017-06-09 16:38:05 +0300

    add ActionResult

---
+++ ASP.NET/MVC5_1/MVC5_1/Util/HtmlResult.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Mvc;
+
+
+// Создадим свои результаты действий. Они будут очень простыми. 
+// Возьмем какой-нибудь проект, например, из прошлой главы, 
+// и добавим в него новую папку Util, которая будет содержать новые классы. 
+// После добавления папки добавим в нее первый класс. Назовем его HtmlResult. 
+// Он у нас будет содержать следующий код: 
+
+namespace MVC5_1.Util
+{
+    public class HtmlResult : ActionResult
+    {
+        private string htmlCode;
+        public HtmlResult(string html)
+        {
+            htmlCode = html;
+        }
+        public override void ExecuteResult(ControllerContext context)
+        {
+            string fullHtmlCode = "<!DOCTYPE html><html><head>";
+            fullHtmlCode += "<title>Главная страница</title>";
+            fullHtmlCode += "<meta charset=utf-8 />";
+            fullHtmlCode += "</head> <body>";
+            fullHtmlCode += htmlCode;
+            fullHtmlCode += "</body></html>";
+            context.HttpContext.Response.Write(fullHtmlCode);
+        }
+    }
+    // В конструкторе класса HtmlResult получаем переданный html-код, 
+    // а в методе Execute вставляем его в общее окружение, 
+    // чтобы получилась полноценная html-страница, 
+    // и пишем ее в выходной поток: context.HttpContext.Response.Write(fullHtmlCode);
+}
---
+++ ASP.NET/MVC5_1/MVC5_1/Util/ImageResult.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+// Создадим еще один класс результатов. Добавим в папку Util новый класс ImageResult:
+namespace MVC5_1.Util
+{
+    public class ImageResult : ActionResult
+    {
+        private string path;
+        public ImageResult(string path)
+        {
+            this.path = path;
+        }
+        public override void ExecuteResult(ControllerContext context)
+        {
+            context.HttpContext.Response.Write("<div style='width:100%;text-align:center;'>" +
+                "<img style='max-width:600px;' src='" + path + "' /></div>");
+        }
+    }
+}
+
+// Данный класс не сложнее предыдущего и просто отдает изображение к html-коде.
+// Тогда метод, использующий данный результат действий, мог бы выглядеть так:
+    //public ActionResult GetImage()
+    //{
+    //    string path = "../Images/visualstudio.png";
+    //    return new ImageResult(path);
+    //}
+// Здесь предполагается, что в проекте есть папка Images, 
+// в которой имеется изображение visualstudio.png.И тогда, 
+// если мы в браузере обратимся к этому действию, например, Home/GetImage, 
+// то сможем увидеть изображение.
