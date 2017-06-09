37023104432157360e7322369d28c048ee94807f
Serhii <semenov@mksat.net>
2017-06-09 18:32:50 +0300

    Add projec

---
+++ ASP.NET/StepShopApplication/StepShop/Controllers/ItemController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using System.Threading.Tasks;
+
+namespace StepShop.Controllers
+{
+    public class ItemController : Controller
+    {
+        // GET: Item
+        public async Task<ActionResult> Datails( int id)
+        {
+            using (ShopEntities entities = new ShopEntities())
+            {
+                ViewBag.Item = await entities.Items.Include(i => i.CategoryType).FirstOrDefaultAsync(i=> i.Id==id);
+                if (ViewBag.Item==null)
+                {
+                    return Redirect("/Home/Index");
+                }
+                ViewBag.Coments = await entities.Comments.Include(i => i.Item).Where(i => i.ItemId == id).OrderByDescending(i=>i.Date).ToListAsync();
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AddComment(Comment comment)
+        {
+            comment.Date = DateTime.Now;
+            using (ShopEntities entities = new ShopEntities())
+            {
+                entities.Comments.Add(comment);
+                entities.SaveChanges();
+            }
+            return RedirectToAction("Datails", "Item", new { id = comment.ItemId });
+        }
+
+    }
+}
