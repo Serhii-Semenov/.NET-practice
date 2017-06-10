9b9f4e0a311efd2cccd4b23fcc17141886498e55
Serhii-Semenov <semenov!@mksat.net>
2017-06-10 19:44:44 +0300

    Add class project StepShop

---
+++ ASP.NET/StepShop/StepShop/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StepShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            using(StepShop.Models.ShopEntities1 entities = new Models.ShopEntities1())
+            {
+                ViewBag.Items = entities.Items
+                                        .Include(i => i.CategoryType)
+                                        .ToList();
+                // TO DO если нет такого id показать вьюху с ошибкой
+            }
+            return View();
+        }
+    }
+}
---
+++ ASP.NET/StepShop/StepShop/Controllers/ItemController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Web.Mvc;
+using StepShop.Models;
+
+namespace StepShop.Controllers
+{
+    public class ItemController : Controller
+    {
+        // GET: Item
+        public ActionResult Details(int id)
+        {
+            // TODO: получить нужный элемент и отдать его представлению
+            using(StepShop.Models.ShopEntities1 entities = new Models.ShopEntities1())
+            {
+                ViewBag.Item = entities.Items
+                    .Include(i => i.CategoryType)
+                    .Include(i => i.Comments)
+                    .FirstOrDefault(a => a.Id == id);
+                // TODO : вывести полную информацию о товаре
+
+                // TODO : добавить отзывы 
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddComment(Comment comment)
+        {
+            // TODO : save comment to DB
+            using (StepShop.Models.ShopEntities1 entities = new Models.ShopEntities1())
+            {
+
+                entities.Comments.Add(comment);
+                entities.SaveChanges();
+
+            }
+
+            return View("Details", comment.ItemId);
+        }
+    }
+}
