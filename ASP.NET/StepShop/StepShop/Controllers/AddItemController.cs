160c50aee46746f37aff81903b542180d98b8fc3
Serhii <semenov@mksat.net>
2017-06-13 20:51:32 +0300

    Class work2

---
+++ ASP.NET/StepShop/StepShop/Controllers/AddItemController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StepShop.Controllers
+{
+    public class AddItemController : Controller
+    {
+
+        public ActionResult AddItemView()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ASP.NET/StepShop/StepShop/Controllers/ChangeItemController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StepShop.Controllers
+{
+    public class ChangeItemController : Controller
+    {
+        // GET: ChangeItem
+        public ActionResult IndexChange(int? id)
+        {
+            using (StepShop.Models.ShopEntities1 entities = new Models.ShopEntities1())
+            {
+                ViewBag.Items = entities.Items.FirstOrDefault(a=>a.Id == id);
+
+                if (ViewBag.Items == null) return View("Error"); // Сделать вьюху ошибки
+            }
+
+
+            return View();
+        }
+    }
+}
