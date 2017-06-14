c3a9d3e6264a5a292a3ce3d11e8e2d5d4a063445
Serhii-Semenov <semenov!@mksat.net>
2017-06-14 16:12:29 +0300

    last home

--- ASP.NET/StepShop/StepShop/Controllers/ChangeItemController.cs
+++ ASP.NET/StepShop/StepShop/Controllers/ChangeItemController.cs
@@ -1,25 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Data.Entity;
 using System.Web.Mvc;
+using System.Threading.Tasks;
+using StepShop.Models;
+using System.Web.UI.WebControls;
+using System.Text;
 
 namespace StepShop.Controllers
 {
     public class ChangeItemController : Controller
     {
         // GET: ChangeItem
-        public ActionResult IndexChange(int? id)
+        [HttpGet]
+        public async Task<ActionResult> Change(int? id = null)
         {
             using (StepShop.Models.ShopEntities1 entities = new Models.ShopEntities1())
             {
-                ViewBag.Items = entities.Items.FirstOrDefault(a=>a.Id == id);
+                var item = await entities.Items
+                    .Include(a => a.CategoryType)
+                    .Include(a => a.ItemImages)
+                    .FirstOrDefaultAsync(a => a.Id == id);
 
-                if (ViewBag.Items == null) return View("Error"); // Сделать вьюху ошибки
-            }
+                if (item == null) return Content("Error", "text/plain", Encoding.UTF8); // Сделать вьюху ошибки
 
+                var selectedList = await entities.CategoryTypes.ToListAsync();
+                ViewBag.SL = new SelectList(selectedList, "Id","Name");
 
-            return View();
+                return View("Change", item);
+            }  
         }
+
+        [HttpPost]
+        public async Task<ActionResult> Change(Item it)
+        {
+            if (it == null) Content("Error", "text/plain", Encoding.UTF8); // Сделать вьюху ошибки
+            using (StepShop.Models.ShopEntities1 entities = new Models.ShopEntities1())
+            {
+                var item = await entities.Items
+                    .FirstOrDefaultAsync(a => a.Id == it.Id);
+
+                if (item == null) return Content("Error", "text/plain", Encoding.UTF8); // Сделать вьюху ошибки
+
+
+
+
+
+
+                return View("Change", item);
+            }
+        }
+
     }
 }
--- ASP.NET/StepShop/StepShop/Controllers/HomeController.cs
+++ ASP.NET/StepShop/StepShop/Controllers/HomeController.cs
@@ -1,22 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Data.Entity;
-using System.Web;
+﻿using System.Data.Entity;
 using System.Web.Mvc;
+using System.Threading.Tasks;
 
 namespace StepShop.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
             using(StepShop.Models.ShopEntities1 entities = new Models.ShopEntities1())
             {
-                ViewBag.Items = entities.Items
+                ViewBag.Items = await entities.Items
                                         .Include(i => i.CategoryType)
-                                        .ToList();
+                                        .ToListAsync();
 
                 if (ViewBag.Items == null) return View("Error");
             }
