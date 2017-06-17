7ca093bb479b77b1a284f00e4a4c47b5f2d57fde
Serhii-Semenov <semenov!@mksat.net>
2017-06-17 22:25:29 +0300

    after sleeping

---
+++ ASP.NET/StepShop/StepShop/Controllers/CategoryTypesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StepShop.Models;
+
+namespace StepShop.Controllers
+{
+    public class CategoryTypesController : Controller
+    {
+        private ShopEntities1 db = new ShopEntities1();
+
+        // GET: CategoryTypes
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.CategoryTypes.ToListAsync());
+        }
+
+        // GET: CategoryTypes/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CategoryType categoryType = await db.CategoryTypes.FindAsync(id);
+            if (categoryType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoryType);
+        }
+
+        // GET: CategoryTypes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CategoryTypes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Name")] CategoryType categoryType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CategoryTypes.Add(categoryType);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(categoryType);
+        }
+
+        // GET: CategoryTypes/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CategoryType categoryType = await db.CategoryTypes.FindAsync(id);
+            if (categoryType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoryType);
+        }
+
+        // POST: CategoryTypes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name")] CategoryType categoryType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(categoryType).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(categoryType);
+        }
+
+        // GET: CategoryTypes/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CategoryType categoryType = await db.CategoryTypes.FindAsync(id);
+            if (categoryType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoryType);
+        }
+
+        // POST: CategoryTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            CategoryType categoryType = await db.CategoryTypes.FindAsync(id);
+            db.CategoryTypes.Remove(categoryType);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ASP.NET/StepShop/StepShop/Controllers/CommentsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StepShop.Models;
+
+namespace StepShop.Controllers
+{
+    public class CommentsController : Controller
+    {
+        private ShopEntities1 db = new ShopEntities1();
+
+        // GET: Comments
+        public async Task<ActionResult> Index()
+        {
+            var comments = db.Comments.Include(c => c.Item);
+            return View(await comments.ToListAsync());
+        }
+
+        // GET: Comments/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comment comment = await db.Comments.FindAsync(id);
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comment);
+        }
+
+        // GET: Comments/Create
+        public ActionResult Create()
+        {
+            ViewBag.ItemId = new SelectList(db.Items, "Id", "Name");
+            return View();
+        }
+
+        // POST: Comments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Author,Message,Date,ItemId")] Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Comments.Add(comment);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ItemId = new SelectList(db.Items, "Id", "Name", comment.ItemId);
+            return View(comment);
+        }
+
+        // GET: Comments/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comment comment = await db.Comments.FindAsync(id);
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ItemId = new SelectList(db.Items, "Id", "Name", comment.ItemId);
+            return View(comment);
+        }
+
+        // POST: Comments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Author,Message,Date,ItemId")] Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(comment).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ItemId = new SelectList(db.Items, "Id", "Name", comment.ItemId);
+            return View(comment);
+        }
+
+        // GET: Comments/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comment comment = await db.Comments.FindAsync(id);
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comment);
+        }
+
+        // POST: Comments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Comment comment = await db.Comments.FindAsync(id);
+            db.Comments.Remove(comment);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
