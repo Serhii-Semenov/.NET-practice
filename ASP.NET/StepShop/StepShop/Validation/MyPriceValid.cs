8547d3584c7aa90caa713b047c9f240c8a24110e
Serhii <semenov@mksat.net>
2017-06-21 20:55:09 +0300

    class work

---
+++ ASP.NET/StepShop/StepShop/Validation/MyPriceValid.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StepShop.Validation
+{
+    public class MyPriceValid : ValidationAttribute
+    {
+        // проверка на не валидные значения
+        private static int[] noValidPrice;
+
+        public MyPriceValid(int[] price)
+        {
+            noValidPrice = price;
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (value != null)
+            {
+                int val = (int)value;
+                foreach(int i in noValidPrice)
+                {
+                    if (val == i) return false;
+                }
+            }
+            return true;
+        }
+    }
+}
