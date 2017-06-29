556a9252e7e55a65e19b3dc5b6e475ba9607ab20
Serhii-Semenov <semenov!@mksat.net>
2017-06-29 17:21:41 +0300

    home2

---
+++ ASP.NET/AuthASP/AuthASP/Controllers/WebApi/ApiItemController.cs
@@ -0,0 +1,31 @@
+﻿using LogicLevel;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+using AuthASP.Mapping;
+using DataLevel.Model;
+using AuthASP.Models;
+using System.Web.Http;
+using System.Linq;
+
+namespace AuthASP.Controllers
+{
+    public class ApiItemController : ApiController
+    {
+        // GET: ApiItem
+        //public async Task<string> Get()
+        //{
+        //    // TODO get item list and send as JSON
+        //    var r = new ItemRepository();
+        //    List<Item> items = await r.GetAllItem();
+        //    List<ShopItem> shopItems = items.ToShopItems();
+        //    var json = Json(shopItems.Select(a => new {Name = a.Name}));
+        //    return json.ToString();
+        //}
+
+        public string Get()
+        {
+            return System.DateTime.Now.ToString();
+        }
+    }
+}
--- ASP.NET/AuthASP/AuthASP/Scripts/Items/shop.js
+++ ASP.NET/AuthASP/AuthASP/Scripts/Items/shop.js
@@ -1,10 +1,25 @@
-﻿function GetItems() {
-    $.get('ApiItem', '', function (items) {
-        alert(items);
-        console.log(items);
-        var it = JSON.parse(items);
-        
-        console.log(it); 
-    })
+﻿
+$(document).ready(function () {
+    $("#button").click(getDate);
+});
+
+function getDate() {
+    var url = "/api/date"
+
+    // Создание запроса по адресу "/api/date" и вывод ответа в элемент c id="date"
+    $.getJSON(url, function (data) {
+        $("#date").text(data);
+        console.log(data);
+    });
 }
+
+//function GetItems() {
+//    $.get('WebApiItem', '', function (items) {
+//        alert(items);
+//        console.log(items);
+//        var it = JSON.parse(items);
+        
+//        console.log(it); 
+//    })
+//}
 //* ДЗ Востанновить JSON объекты на клиенте и востановит х на VIEW
